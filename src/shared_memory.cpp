f3b0e665f585f02e362e0082e14483762ee8d01c
shecannotsee <shecannotsee@163.com>
2024-02-26 18:52:35 +0800

    [feat] build raft project

---
+++ src/shared_memory.cpp
@@ -0,0 +1,7 @@
+//
+// Created by shecannotsee on 24-2-26.
+//
+
+#include "shared_memory.h"
+
+namespace she_raft {}  // namespace she_raft
---
+++ src/shared_memory.h
@@ -0,0 +1,14 @@
+//
+// Created by shecannotsee on 24-2-26.
+//
+
+#ifndef SHARED_MEMORY_H
+#define SHARED_MEMORY_H
+
+namespace she_raft {
+
+class shared_memory {};
+
+}  // namespace she_raft
+
+#endif  // SHARED_MEMORY_H
